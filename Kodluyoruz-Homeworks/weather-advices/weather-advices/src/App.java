1be8a6664dc4fc9a77d982e6e0c04355d6857a56
Batuhan <batuhancelebi.business@gmail.com>
2023-04-27 12:50:42 +0300

    Weather Advices added

---
+++ Kodluyoruz-Homeworks/weather-advices/weather-advices/src/App.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hava durumunu giriniz:");
+        int heat = scanner.nextInt();
+
+        if (heat < 5) {
+            System.out.println("Kayak yapabilirsiniz.");
+        } else if ((heat >= 5) && (heat <= 15)) {
+            System.out.println("Sinemaya gidebilirsiniz");
+        } else if ((heat > 15) && (heat <= 25)) {
+            System.out.println("Pikniğe gidebilirsiniz.");
+        } else if (heat > 25) {
+            System.out.println("Yüzmeye gidebilirsiniz.");
+        }
+
+    }
+}
