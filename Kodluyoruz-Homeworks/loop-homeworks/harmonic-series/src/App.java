12478c77d9ae7d7c90030b40b006d7ffe0013792
Batuhan <batuhancelebi.business@gmail.com>
2023-05-01 15:24:28 +0300

    harmonics-series added

---
+++ Kodluyoruz-Homeworks/loop-homeworks/harmonic-series/src/App.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("...Harmonik seri uygulaması...");
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Bir sayı giriniz: ");
+        int input = scn.nextInt();
+
+        double toplam = 1;
+        for (double i = 1; i <= input; i++) {
+            toplam += (1 / i);
+        }
+        System.out.println(toplam);
+    }
+}
