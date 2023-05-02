3949087850a23dec74ff9fb41fa2b919f01b5a2e
Batuhan <batuhancelebi.business@gmail.com>
2023-05-02 14:36:58 +0300

    ebob ekok finder

---
+++ Kodluyoruz-Homeworks/loop-homeworks/ebob-ekok-finder/README.md
@@ -0,0 +1,1 @@
+## simple ebob-ekok calculator
---
+++ Kodluyoruz-Homeworks/loop-homeworks/ebob-ekok-finder/src/App.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Küçük olan sayıyı giriniz: ");
+        int n1 = scn.nextInt();
+        System.out.println("Büyük olan sayıyı giriniz: ");
+        int n2 = scn.nextInt();
+
+        // EBOB
+        int ebob = 1;
+        int ekok = 0;
+        int i = 1;
+        while (i < n1) {
+            if (n1 % i == 0 && n2 % i == 0) {
+                ebob = i;
+            }
+            i++;
+        }
+        System.out.println(ebob);
+        i = n1;
+        while (++i < (n1 * n2)) {
+            if (i % n1 == i % n2) {
+                ekok = i;
+                break;
+            }
+        }
+        System.out.println("ekok:" + ekok);
+    }
+}
